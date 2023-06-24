4e97a9af28f56fe1067c5f5b2bc9f442eced7cdb
hong2HSH <tnghd1717@gmail.com>
2023-06-24 23:45:16 +0900

    230624

--- python/BOJ/2470.py
+++ python/BOJ/2470.py
@@ -1,10 +1,47 @@
-n = int(input())
-plus = []
-minus = []
+# import sys
 
-for i in n:
-    if i > 0:
-        plus.append(i)
+# input = sys.stdin.readline
+# n = int(input())
+# plus = []
+# minus = []
+# li = list(map(int, input().split()))
+
+# for i in li:
+#     if i > 0:
+#         plus.append(i)
+#     else:
+#         minus.append(i)
+
+# sum = plus[0] + minus[0]
+# li = []
+
+# for i in plus:
+#     for j in minus:
+#         if i + j < abs(sum):
+#             sum = i + j
+#             li = [j, i]
+# print(" ".join(map(str, li)))
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+li = list(map(int, input().split()))
+li.sort()
+sum = abs(li[0] + li[1])
+left = 0
+right = n - 1
+ans = [li[0], li[1]]
+
+while left < right:
+    sum1 = li[left] + li[right]
+    if abs(sum1) < sum:
+        sum = abs(sum1)
+        ans = [li[left], li[right]]
+    if sum1 < 0:
+        left += 1
     else:
-        minus.append(i)
-print(plus, minus)
+        right -= 1
+print(ans[0], ans[1])
---
+++ python/BOJ/5052.py
@@ -0,0 +1,9 @@
+t = int(input())
+li = []
+
+for _ in range(t):
+    a = int(input())
+    for i in range(a):
+        e = int(input())
+        for j in range(a-1):
+            
