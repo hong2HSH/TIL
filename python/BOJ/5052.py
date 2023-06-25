c8431a3e23184f334ea23509d08ceab0895f7d3b
hong2HSH <tnghd1717@gmail.com>
2023-06-25 21:56:23 +0900

    230625

--- python/BOJ/5052.py
+++ python/BOJ/5052.py
@@ -1,9 +1,16 @@
 t = int(input())
-li = []
 
 for _ in range(t):
     a = int(input())
-    for i in range(a):
-        e = int(input())
-        for j in range(a-1):
-            
+    e = list(map(int, str(input())))
+    for i in range(a - 1):
+        cnt = 0
+        b = list(map(int, str(input())))
+        for j in range(3):
+            if e[j] == b[j]:
+                cnt += 1
+        if cnt == 3:
+            print("NO")
+            break
+    if cnt != 3:
+        print("YES")
