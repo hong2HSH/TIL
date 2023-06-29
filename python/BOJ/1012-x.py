871b3cc1fb0ccfcd250d03e14708f9a9bd05ddce
hong2HSH <tnghd1717@gmail.com>
2023-06-29 23:36:16 +0900

    230629

--- python/BOJ/1012-x.py
+++ python/BOJ/1012-x.py
@@ -1,7 +1,10 @@
 t = int(input())
+
+
 for _ in range(t):
     m, n, k = map(int, input().split())
     graph = [[0 for _ in range(m)] for _ in range(n)]
 
     for _ in range(k):
         x, y = map(int, input().split())
+        graph[x][y] = 1
